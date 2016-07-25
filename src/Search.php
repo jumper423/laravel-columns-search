b3eb59ef75689219f78f391805898e2a9c83fec8
jumper423 <jump.e.r@yandex.ru>
2016-07-25 14:30:24 +0300

    column type

--- src/Search.php
+++ src/Search.php
@@ -4,14 +4,13 @@ namespace jumper423\LaravelTrait;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Schema;
 
 /**
  * Trait Sortable.
  */
 trait Search
 {
-    protected $searchColumns = [];
-
     /**
      * @param Builder $query
      *
@@ -21,13 +20,39 @@ trait Search
     {
         if (Input::has('q') && count($this->searchColumns)) {
             $q = Input::get('q');
-            $searchColumns = $this->searchColumns;
-            $query->where(function ($query2) use ($q, $searchColumns) {
-                foreach ($searchColumns as $searchColumn) {
-                    $query2->orWhere($searchColumn, 'ilike', "%{$q}%");
+            $model = $this;
+            $query->where(function ($query2) use ($q, $model) {
+                foreach ($model->getColumns() as $searchColumn) {
+                    switch (Schema::getColumnType($model->getTable(), $searchColumn)) {
+                        case 'integer':
+                        case 'bigint':
+                        case 'smallint':
+                        case 'float':
+                            if (is_numeric($q)) {
+                                $query2->orWhere($searchColumn, '=', (int)$q);
+                            }
+                            break;
+                        case 'string':
+                        case 'text':
+                            $query2->orWhere($searchColumn, 'ilike', "%{$q}%");
+                            break;
+                    }
+
                 }
             });
         }
         return $query;
+    }
+
+    /**
+     * @return array
+     */
+    private function getColumns()
+    {
+        if (!isset($this->searchColumns)) {
+            return [];
+        } else {
+            return $this->searchColumns;
+        }
     }
 }
