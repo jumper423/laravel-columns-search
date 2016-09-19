f4478044de70dbf4aefdc30038c0fb9664462213
Влад Пасечник <jump.e.r@yandex.ru>
2016-09-19 15:01:42 +0300

    Update Search.php
    поиск по присоединённым таблицам

--- src/Search.php
+++ src/Search.php
@@ -23,18 +23,24 @@ trait Search
             $model = $this;
             $query->where(function ($query2) use ($q, $model) {
                 foreach ($model->getColumns() as $searchColumn) {
-                    switch (Schema::getColumnType($model->getTable(), $searchColumn)) {
+                    $table = $model->getTable();
+                    $searchColumnExplode = explode('.', $searchColumn);
+                    if (count($searchColumnExplode) > 1) {
+                        $table = $searchColumnExplode[0];
+                        $searchColumn = $searchColumnExplode[1];
+                    }
+                    switch (Schema::getColumnType($table, $searchColumn)) {
                         case 'integer':
                         case 'bigint':
                         case 'smallint':
                         case 'float':
                             if (is_numeric($q)) {
-                                $query2->orWhere($searchColumn, '=', (int)$q);
+                                $query2->orWhere($table . '.' . $searchColumn, '=', (int)$q);
                             }
                             break;
                         case 'string':
                         case 'text':
-                            $query2->orWhere($searchColumn, 'ilike', "%{$q}%");
+                            $query2->orWhere($table . '.' . $searchColumn, 'ilike', "%{$q}%");
                             break;
                     }
 
